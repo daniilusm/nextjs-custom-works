80374c96a94ce2a4eddf85efa264ee226193ff2a
Daniil Usmantsev <daniil.usmantsev@gmail.com>
2023-01-17 11:15:45 +0300

    created prllx

--- pages/parallax/parallax.styled.ts
+++ pages/parallax/parallax.styled.ts
@@ -1,10 +1,49 @@
 import styled from 'styled-components';
 
-export const Root = styled.div<{ bcg: any }>`
-  background-image: ${({ bcg }) => bcg && `url(${bcg.src})`};
+export const Root = styled.div`
+  height: 100%;
+`;
+
+export const Layer = styled.div<{ bckImg: any }>`
+  background-image: ${({ bckImg }) => bckImg && `url(${bckImg.src})`};
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  background-size: cover;
+  background-position: center;
+  will-change: transform;
+  transition: transform 0.75s cubic-bezier(0.075, 0.5, 0, 1);
+`;
+
+export const BaseLayer = styled(Layer)`
+  transform: translate3d(0, calc(var(--scrollTop)/ 1.6), 0)};
+  z-index: 0;
+`;
+
+export const FrontLayer = styled(Layer)`
+transform: translate3d(0, calc(var(--scrollTop)/ 5.7), 0)};
+z-index: 2;
+`;
+
+export const MiddleLayer = styled(Layer)`
+transform: translate3d(0, calc(var(--scrollTop)/ 2.5), 0)};
+z-index: 1;
+`;
+
+export const BackLayer = styled(Layer)`
+transform: translate3d(0, calc(var(--scrollTop)/ 1.6), 0)};
+z-index: 0;
+`;
+
+export const Layers = styled.div`
+  position: relative;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
 `;
 
 export const Block = styled.div`
-  width: 100vw;
-  height: 100vw;
+  height: 100vh;
 `;
--- pages/parallax/parallax.tsx
+++ pages/parallax/parallax.tsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 import gsap from 'gsap-trial';
 import { ScrollTrigger } from 'gsap-trial/dist/ScrollTrigger';
 import { ScrollSmoother } from 'gsap-trial/dist/ScrollSmoother';
-import bg from 'assets/images/photo_2022-12-27_14-23-01.jpg';
-import bg2 from 'assets/images/photo_2022-12-27_14-23-11.jpg';
-import bg3 from 'assets/images/photo_2022-12-27_14-23-04.jpg';
+import imgBase from 'assets/images/main-img/base.jpeg';
+import imgFront from 'assets/images/main-img/witcher-front.png';
+import imgMiddle1 from 'assets/images/main-img/witcher-middle-1.png';
+import imgMiddle2 from 'assets/images/main-img/witcher-middle-2.png';
+import imgBack from 'assets/images/main-img/witcher-back.png';
 
 import * as S from './parallax.styled';
 import { StaticImageData } from 'next/image';
@@ -14,65 +16,72 @@ const sections = ['first', 'second', 'third'];
 const parallax = () => {
   gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-  const [activeSection, setActiveSection] = useState('first');
-  const [backgroundColor, setBackgroundColor] = useState<StaticImageData>(bg);
+  const [windowScrollTop, setWindowScrollTop] = useState(0);
+  // const [backgroundColor, setBackgroundColor] = useState<StaticImageData>(bg);
 
   useEffect(() => {
-    const smoother = ScrollSmoother.create({
-      content: '.smooth-wrapper',
-      smooth: 3,
-      effects: true,
+    // const smoother = ScrollSmoother.create({
+    //   content: '.smooth-content',
+    //   smooth: 3,
+    //   effects: true,
+    // });
+    window.addEventListener('scroll', () => {
+      // console.info(window.scrollY);
+      setWindowScrollTop(window.scrollY);
+      document.documentElement.style.setProperty(
+        '--scrollTop',
+        `${window.scrollY}px`
+      );
     });
 
-    // const smoother = ScrollSmoother.create({
-    //   wrapper: '.smooth-wrapper',
-    //   content: '.smooth-content',
-    // });
+    const smoother = ScrollSmoother.create({
+      wrapper: '.smooth-wrapper',
+      content: '.smooth-content',
+    });
 
     smoother.effects('img', { speed: 'auto' });
 
-    sections.forEach((section) => {
-      gsap.to(`#${section}`, {
-        scrollTrigger: {
-          trigger: `#${section}`,
-          start: 'top center',
-          end: 'bottom center',
-          onEnter: () => setActiveSection(`${section}`),
-          onEnterBack: () => setActiveSection(`${section}`),
-        },
-      });
-    });
+    // sections.forEach((section) => {
+    //   gsap.to(`#${section}`, {
+    //     scrollTrigger: {
+    //       trigger: `#${section}`,
+    //       start: 'top center',
+    //       end: 'bottom center',
+    //       onEnter: () => setActiveSection(`${section}`),
+    //       onEnterBack: () => setActiveSection(`${section}`),
+    //     },
+    //   });
+    // });
   }, []);
 
-  useEffect(() => {
-    switch (activeSection) {
-      case 'first':
-        setBackgroundColor(bg);
-        break;
-      case 'second':
-        setBackgroundColor(bg);
-        break;
-      case 'third':
-        setBackgroundColor(bg2);
-        console.log('change');
-        break;
-      default:
-        setBackgroundColor(bg);
-    }
-  }, [activeSection]);
+  // useEffect(() => {
+  //   switch (activeSection) {
+  //     case 'first':
+  //       setBackgroundColor(bg);
+  //       break;
+  //     case 'second':
+  //       setBackgroundColor(bg);
+  //       break;
+  //     case 'third':
+  //       setBackgroundColor(bg2);
+  //       console.log('change');
+  //       break;
+  //     default:
+  //       setBackgroundColor(bg);
+  //   }
+  // }, [activeSection]);
 
   return (
-    <S.Root bcg={backgroundColor} className="smooth-wrapper">
+    <S.Root className="smooth-wrapper">
       <div className="smooth-content">
-        <S.Block id="first">
-          <div>1</div>
-        </S.Block>
-        <S.Block id="second">
-          <div>2</div>
-        </S.Block>
-        <S.Block id="third">
-          <div>3</div>
-        </S.Block>
+        <S.Layers>
+          {/* <S.BaseLayer bckImg={imgBase} /> */}
+          <S.FrontLayer bckImg={imgFront} />
+          <S.MiddleLayer bckImg={imgMiddle1} />
+          <S.MiddleLayer bckImg={imgMiddle2} />
+          <S.BackLayer bckImg={imgBack} />
+        </S.Layers>
+        <S.Block>content</S.Block>
       </div>
     </S.Root>
   );
