8635bc8be78c4752178727bc7dad52ac9e4c6d83
Daniil Usmantsev <daniil.usmantsev@gmail.com>
2023-01-24 17:35:16 +0300

    some changes

--- pages/animations/index.tsx
+++ pages/animations/index.tsx
@@ -1,7 +1,9 @@
+import { Canvas } from '@react-three/fiber';
 import Button from 'components/Button';
+import { Model } from 'components/Canvas/Totoro';
 import BasicLayout from 'components/Layouts/Basic';
 import Head from 'next/head';
-import React from 'react';
+import React, { Suspense } from 'react';
 import * as S from 'styles/pages/animations.styled';
 
 const Animations = () => {
@@ -14,6 +16,13 @@ const Animations = () => {
         <S.Root>
           <Button withGradient>open</Button>
         </S.Root>
+        <Canvas className="canvas">
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[-2, 5, 2]} />
+          <Suspense fallback={null}>
+            <Model />
+          </Suspense>
+        </Canvas>
       </BasicLayout>
     </>
   );
